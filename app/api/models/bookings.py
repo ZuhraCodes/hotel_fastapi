1fdbd88baf35f89fb521694a3fcbe826825744bf
BotirIslomboev <=>
2024-12-18 09:48:04 +0500

    feat(core): add models and alembic in project $2

--- app/api/models/bookings.py
+++ app/api/models/bookings.py
@@ -9,7 +9,7 @@ from app.core.models.base import Base
 class Bookings(Base):
     __tablename__ = 'bookings'
 
-    user_id: Mapped[int] = mapped_column(ForeignKey('users.id', ondelete='SET NULL'), primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id', ondelete='SET NULL'))
     room_id: Mapped[int] = mapped_column(ForeignKey('rooms.id', ondelete="CASCADE"))
     check_in: Mapped[datetime]
     check_out: Mapped[datetime]
