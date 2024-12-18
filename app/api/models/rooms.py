84df092c0497d48d2043f3c764354be2b273b7f9
BotirIslomboev <=>
2024-12-18 10:24:53 +0500

    feat(core): add models and alembic in project #2

--- app/api/models/rooms.py
+++ app/api/models/rooms.py
@@ -1,4 +1,5 @@
-from sqlalchemy.orm import Mapped
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
 
 from app.core.models.base import Base
 
@@ -7,6 +8,18 @@ class Rooms(Base):
     __tablename__ = 'rooms'
 
     room_number: Mapped[int]
-    room_type: Mapped[str]
+    room_type: Mapped[int] = mapped_column(ForeignKey('rooms_type.id'))
     price: Mapped[float]
-    status: Mapped[str]
+    status: Mapped[int] = mapped_column(ForeignKey('rooms_status.id'))
+
+
+class RoomsType(Base):
+    __tablename__ = 'rooms_type'
+
+    title: Mapped[str]
+
+
+class RoomsStatus(Base):
+    __tablename__ = 'rooms_status'
+
+    title: Mapped[str]
